a6b0691daa1dde1c9bd8d4ef0e90df497e74d483
OkHu-MC <108510544+OkHu-MC@users.noreply.github.com>
2025-04-29 21:15:55 +0900

    s

---
+++ C_01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+//#include <jmpset.h>
+#include <math.h>
+
+int main(void);
+void C_01(void){
+    printf("C_01\n");
+    printf("Hello, World!\n");
+}
+
+
+int main(void){
+    C_01();
+    return 0;
+}
