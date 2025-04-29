771eb099d10ed06c01dfdd19069889ae31da1d10
OkHu-MC <108510544+OkHu-MC@users.noreply.github.com>
2025-04-29 22:01:37 +0900

    勉強

--- C_01.c
+++ C_01.c
@@ -2,17 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
 //#include <jmpset.h>
 #include <math.h>
-
+#include <time.h>
 int main(void);
 void C_01(void){
     printf("C_01\n");
     printf("Hello, World!\n");
 }
-
+int C_02(void){
+    int i = 0;
+    for (i = 0; i<INT_MAX; i++){
+            
+        if (i%10000000==0){
+            sleep(1);
+            printf("\r%d", i);
+        } 
+    }
+    return 0;
+}
 
 int main(void){
     C_01();
+    C_02();
     return 0;
 }
